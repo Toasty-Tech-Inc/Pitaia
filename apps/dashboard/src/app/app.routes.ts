78f339e05a128008e6cbc2b38d2f0635b742d84c
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-10-26 23:48:49 -0300

    fix: ajuste de rotas no dashboard

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login').then((m) => m.Login),
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+    },
+];
