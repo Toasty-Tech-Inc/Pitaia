240f9a753d190363f5bce60bdd67b016e5505ff3
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-11-03 17:22:03 -0300

    feat: adiciona documentação

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,11 +6,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pitaia API')
+    .setDescription('Api principal de gerenciamento de produtos, pedidos e usuarios')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
