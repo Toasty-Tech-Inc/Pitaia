2b4412900d67b71f64af1b99f58c52ad29d3439f
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-11-19 19:40:10 -0300

    feat: integrate @scalar/nestjs-api-reference for API documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -32,6 +33,13 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.use(
+    '/reference',
+    apiReference({
+      content: documentFactory(),
+    }),
+  )
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
