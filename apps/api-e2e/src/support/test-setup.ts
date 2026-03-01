82892943574e8210cbc15e7e17da97199015e649
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2026-03-01 11:50:20 -0300

    ffffix: e2e testes

--- apps/api-e2e/src/support/test-setup.ts
+++ apps/api-e2e/src/support/test-setup.ts
@@ -1,18 +1,17 @@
 /* eslint-disable */
 import axios from 'axios';
 
-module.exports = async function () {
-  // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
-  axios.defaults.baseURL = `http://${host}:${port}/api`;
-  
-  // Configurar timeout padrão
-  axios.defaults.timeout = 10000;
-  
-  // Não lançar erro para status 4xx e 5xx (para poder testar erros)
-  axios.defaults.validateStatus = () => true;
-  
-  console.log('Setting up...');
-  console.log(`Connecting on ${host}:${port}/api`);
-};
+// Configure axios for tests to use.
+// This code runs directly when the file is loaded (setupFiles)
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ?? '3000';
+axios.defaults.baseURL = `http://${host}:${port}/api`;
+
+// Configurar timeout padrão
+axios.defaults.timeout = 10000;
+
+// Não lançar erro para status 4xx e 5xx (para poder testar erros)
+axios.defaults.validateStatus = () => true;
+
+console.log('Setting up...');
+console.log(`Connecting on ${host}:${port}/api`);
