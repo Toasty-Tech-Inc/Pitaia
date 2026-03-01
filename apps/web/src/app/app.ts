a404e364b4c3180ef2de3d213bc8acaef7860311
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2026-03-01 16:10:30 -0300

    fix: scroll web

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {TuiRoot} from '@taiga-ui/core';
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
-  imports: [ RouterModule, TuiRoot],
+  imports: [RouterModule, TuiRoot],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected title = 'web';
